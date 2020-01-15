59551b0f40de3a5f5b71259b2689d809012fd717
Per Lundberg <perlun@gmail.com>
2020-01-15 22:55:36 +0200

    Add code for all of chapter 4
    We now have a working REPL, at least from a scanning/lexing point of
    view. More to come, hopefully soon!

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -14,7 +14,7 @@ public class Lox {
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
-            System.exit(64); ​
+            System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
         } else {
@@ -34,7 +34,7 @@ public class Lox {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for (; ; ) { ​
+        for (; ; ) {
             System.out.print("> ");
             run(reader.readLine());
             hadError = false;
